56434eae7dcbaef858f977370b79d4971da8f205
thecarinha <joaolima15257@gmail.com>
2025-08-27 16:24:59 -0400

    Update ola-mundo.c

--- ola-mundo.c
+++ ola-mundo.c
@@ -1,5 +1,12 @@
+// abaixo, a declaracao da biblioteca que contém as declarações das funções de entrada e saída padrão, como printf e scanf.
 #include<stdio.h>
+/*sem ela, printf nao funcionaria por exemplo*/
+//abaixo, o inicio de um programar, 
 int main(){
+/*abaixo, funcao de saida, onde printf e a funcao,("ola mundo") sera o que vai sair no dispositivo de saida(monitor ex) onde sera apenas exibido ola mundo.*/
 	printf("ola mundo");
+//nao esqueca do ponto e virgula no final.
+//abaixo,e como dizer eu ja acabei com o programar, com return 0 mais ponto e virgula
 	return 0;
+//abaixo, fim do programar
 }
