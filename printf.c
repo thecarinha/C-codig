7e1b7ff6e83988c4d5a195f64f3235f8fbd036a8
thecarinha <joaolima15257@gmail.com>
2025-08-28 22:55:51 -0400

    printf e scanf
    saida e entrada de dados, começo.

---
+++ printf.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+	printf("mensagem"); //esse comando e uma saida de dados,nesse caso a saida e uma mensagem
+	return 0;
+}
---
+++ scanf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int x;
+	printf("de um valor ao numero x: ");
+	scanf(" %d",&x); /*esse comando e uma entrada de dados,nesse caso ele esperar um valor "%d" que e um numero inteiro e vai armazenar na variavel acompanhnado 
+com & que nesse caso e a variavel x. o usuario que vai fornecer a entrada de dados atraves da escrita*/
+printf("%d",x);
+}
